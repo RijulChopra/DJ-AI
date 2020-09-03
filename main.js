98888bfe34e897f8131eab61e190642c15ee17ee
RijulChopra <70279351+RijulChopra@users.noreply.github.com>
2020-09-03 17:01:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 song = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+
 function preload()
 {
     song = loadSound("believer.mp3");
@@ -14,9 +21,33 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
     image(video, 0, 0, 300, 250);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet Is Loaded!!");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log('Left Wrist X = ' + leftWristX + ' Left Wrist Y = ' + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('Right Wrist X = ' + rightWristX + ' Right Wrist Y = ' + rightWristY);
+    }
 }
