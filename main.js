ec42d07c67521c869d714b6a73f86070b511dc43
RijulChopra <70279351+RijulChopra@users.noreply.github.com>
2020-09-11 17:24:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload()
 {
@@ -29,11 +31,23 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 300, 250);
+
+    fill('#ffaaee');
+    stroke('#ffaaee');
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song.play();
+        song.setVolume(10);
+        document.getElementById('song_name').innerHTML = "Song Name : Believer";
+        song2.stop();
+    }
 }
 
 function modelLoaded()
 {
-    console.log("PoseNet Is Loaded!!");
+    console.log('PoseNet Is Loaded!!');
 }
 
 function gotPoses(results)
@@ -41,6 +55,12 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score Right Wrist = " + scoreRightWrist);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("Score Left Wrist = " + scoreLeftWrist);
         
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
